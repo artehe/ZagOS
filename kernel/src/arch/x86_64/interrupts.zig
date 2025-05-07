556e362dad8aaec90187868eb8036e0118175f7d
artehe <112902041+artehe@users.noreply.github.com>
2025-05-07 15:38:59 +0100

    add interrupts enable function

--- kernel/src/arch/x86_64/interrupts.zig
+++ kernel/src/arch/x86_64/interrupts.zig
@@ -2,3 +2,8 @@
 pub inline fn disable() void {
     asm volatile ("cli");
 }
+
+/// Enables interrupts allowing them to be generated and processed by the IDT
+pub inline fn enable() void {
+    asm volatile ("sti");
+}
